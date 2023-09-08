b6258f79d0dc83c66e0904d160d7ca41a24668dd
Araf <arafathossain.baiust@gmail.com>
2023-09-09 02:00:46 +0600

    first commit

---
+++ src/components/Home/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import useSomething from '@/hooks/useSomething';
+const Hero = () => {
+    return (
+        <div>
+            <h1>Home</h1>
+        </div>
+    );
+};
+
+export default Hero;
